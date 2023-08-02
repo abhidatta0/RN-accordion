9f90d8a0474708731b8bd402a2727735d3d0e789
abhidatta0 <abhidatta146@gmail.com>
2023-08-02 23:20:49 +0530

    Layout animation

--- App.tsx
+++ App.tsx
@@ -2,24 +2,44 @@ import React, {useState} from 'react';
 import {
   Text,
   SafeAreaView,
+  UIManager,LayoutAnimation,
+  Platform,
+  StyleSheet,
+  View,
 } from 'react-native';
 import Accordion from './src/components/Accordion';
 
+if(Platform.OS === 'android'){
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 const App = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const titles = ['New York', 'Bangalore','New Delhi', 'Kolkata'];
   const areas = [100,70, 80, 50];
+
+  const updateIndex = (id: number)=> {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+    setActiveIndex(id);
+  }
 
   const getContent = ()=> {
     return <Text>{titles[activeIndex]} has an area of {areas[activeIndex]} square km</Text>
   }
   return (
     <SafeAreaView>
-      <Accordion titles={titles} setActiveIndex={setActiveIndex} activeIndex={activeIndex}> 
+      <Accordion titles={titles} setActiveIndex={updateIndex} activeIndex={activeIndex}> 
+        <View style={styles.content}>
         {getContent()}
+        </View>
         </Accordion>
     </SafeAreaView>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  content: {
+    minHeight:100,
+  }
+})
